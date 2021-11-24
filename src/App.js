aec03823f6d5ac14a2ab4722f1741db5e4e6d968
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-11-24 19:30:06 +0200

    добавил маршруты

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./views/HomePageView/HomePage";
+import MoviesPage from "./views/MoviesPageView/MoviesPage";
+import MovieDetailsPage from "./views/MovieDetailsPageView/MovieDetailsPage";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="movies/" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
